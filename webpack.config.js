a70cbacdba84966b7e4fe9a929c89f5270ef07e6
Simonhil <sihi@outlook.de>
2022-12-30 14:04:37 +0100

    mehr reactserver

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+
+const path = require("path")
+const HTMLWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    entry: "./index.tsx",
+    
+    resolve: {
+        extensions: [
+            ".js", ".json", ".tsx", ".ts"
+        ]
+    },
+
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "bundle.js"  
+    },
+
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./index.html"
+        })
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /.tsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env","@babel/preset-react"
+                        ],
+                    }
+                }
+            }
+        ]
+    }
+}

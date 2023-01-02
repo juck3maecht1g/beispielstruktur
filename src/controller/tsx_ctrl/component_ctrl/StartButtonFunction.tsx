59bf3b11edcdac1a048ee7442f6910838d334785
Simonhil <sihi@outlook.de>
2023-01-02 19:11:26 +0100

    sgs

--- src/controller/tsx_ctrl/component_ctrl/StartButtonFunction.tsx
+++ src/controller/tsx_ctrl/component_ctrl/StartButtonFunction.tsx
@@ -1,5 +1,9 @@
-function StartButtonFunction() {
+export function StartButtonFunction() {
     console.log("yes")
     
+}
+
+export function GetStartButtonInfo() {
+    return "[Name]"
 }
 export default StartButtonFunction;

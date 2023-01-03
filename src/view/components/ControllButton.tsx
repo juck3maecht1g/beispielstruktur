50764357187c3b4d8f6320b2c1dbda926926de79
Simonhil <sihi@outlook.de>
2023-01-03 11:08:57 +0100

    1.3. 11.8
    Co-authored-by: Paul Hendriks <Paul-Hendriks@users.noreply.github.com>

--- src/view/components/ControllButton.tsx
+++ src/view/components/ControllButton.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ControllButtonFunction from '../../controller/tsx_ctrl/component_ctrl/ControllButtonFunction';
+import { GripperButtonFunctionShort, GetGripperButtonInfo } from '../../controller/tsx_ctrl/component_ctrl/GripperButtonFunctions';
 
 export function ControllButton() {
+    let ips = GetGripperButtonInfo()
+    
     return (
-        <button className="ControllButton" onClick={ControllButtonFunction}>Controll</button>
+        <button className="ControllButton" onClick={GripperButtonFunctionShort}>Controll + {ips}</button>
     );
 }
--- src/view/components/StartButton.tsx
+++ src/view/components/StartButton.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import StartButtonFunction from '../../controller/tsx_ctrl/component_ctrl/StartButtonFunction';
+import {StartButtonFunction, GetStartButtonInfo} from '../../controller/tsx_ctrl/component_ctrl/StartButtonFunction';
+import ForFetchTest from '../../controller/tsx_ctrl/forFetchTest';
 import './Styles.css';
 
 export function StartButton() {
+    var name = GetStartButtonInfo();
     return (
-        <button className="StartButton" onClick={StartButtonFunction}>Start</button>
+        <button className="StartButton" onClick={StartButtonFunction}>{"start " + name}</button>
     );
 }

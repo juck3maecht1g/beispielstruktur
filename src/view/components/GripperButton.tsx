a1bb95bcb235dad76345776de0ef54626c00a82a
Simonhil <sihi@outlook.de>
2023-01-03 11:30:27 +0100

    gripper not in controll

--- src/view/components/GripperButton.tsx
+++ src/view/components/GripperButton.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import GripperButtonFunction from '../../controller/tsx_ctrl/component_ctrl/GripperButtonFunction';
 import './Styles.css';
 import {Link} from "react-router-dom";
+import { GetGripperButtonInfo } from '../../controller/tsx_ctrl/component_ctrl/GripperButtonFunctions';
 
 export function GripperButton() {
+    let ips = GetGripperButtonInfo()
     return (
-            <button id="GripperButton" className="Button" onClick={GripperButtonFunction}>Gripper</button>
+            <button id="GripperButton" className="Button" onClick={GripperButtonFunction}>Gripper   {ips}</button>
     );
 }

7a3dfeba076561a5f328e7e38ab5fac7b0b0f495
ujgoa <ujgoa@student.kit.edu>
2023-01-04 17:32:37 +0100

    Co-authored-by: juck3maecht1g <juck3maecht1g@users.noreply.github.com>

--- src/view/BurgerMenü.tsx
+++ src/view/BurgerMenü.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './components/Styles.css';
+import ChooseButton from './components/ChooseButton';
 
 function Burgermenü() {
+    let button = new ChooseButton();
+
     return (
-        <h1 className="Page">
-            <div>
-                This is Burgermenü
-            </div>
+        <h1>
+            <button.render></button.render>
         </h1>
     );
+
 }
 
 export default Burgermenü
---
+++ src/view/components/ChooseButton.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default class ChooseButton extends React.Component {
+    
+    render() {
+      return <button>jhg</button>;
+    }
+  };

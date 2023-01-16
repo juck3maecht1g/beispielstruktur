40a40148d4801f81f5f6d90fe5f56cc66c5d1d0d
uyzqq <uyzqqstudent.kit.edu>
2023-01-16 09:36:06 +0100

    view implementierung anfang

---
+++ src/view.2/steeringPages/setPositionPage/ChooseCurrentPositionButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class ChooseCurrentPositionButton extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return (  
+            <div>
+                
+            </div>
+        );
+    }
+}
+ 
+export default ChooseCurrentPositionButton
---
+++ src/view.2/steeringPages/steeringRobotPage/SteeringRobotPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class SteeringRobotPage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {  }
+    render() { 
+        return (  
+            <div>
+                
+            </div>
+        );
+    }
+}
+ 
+export default SteeringRobotPage;
